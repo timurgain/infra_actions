e9ec7b2861fd4ef52fd4d190108fd3429a7c850d
Timur Gain <timur.gain@gmail.com>
2022-05-24 08:04:47 +0300

    workflow .yml

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'infra_app'
 
-# comment for testing workflows
+# comments for testing workflows
 urlpatterns = [
     path('', views.index, name='index'),
     path('second/', views.second_page, name='second_page'),
